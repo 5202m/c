cbe1b63847dda29dc4695187b2852c8639b0961d
Eugene.Zeng <eugene.zeng@gwtsz.net>
2017-03-01 15:13:35 +0800

    Changed the way call "doRequestSuccess", to avoid data=0 problem.

--- service/liveRoomAPIService.js
+++ service/liveRoomAPIService.js
@@ -1,13 +1,58 @@
 "use strict";
-const Deferred = require("../util/common").Deferred;
-const request = require('request');
-const config = require('../resources/config'); // 引入config
-const logger = require('../resources/logConf').getLogger("liveRoomAPIService");
+let Deferred = require("../util/common").Deferred;
+let request = require('request');
+let config = require('../resources/config'); // 引入config
+let logger = require('../resources/logConf').getLogger("liveRoomAPIService");
 
 const baseUrl = config.pmApiUrl;
 
+let addCompanyIdToPath = path => {
+    let companIdParam = "companyId=" + config.companyId;
+    let params = path.split("?")[1];
+    path += params ? "&" + companIdParam : "?" + companIdParam;
+    return path;
+};
+
+let addCompanyIdToBody = body => {
+    body.companyId = config.companyId;
+    return body;
+}
+
+let doRequestSuccess = (data, defer) => {
+    let dataObj = {};
+    try {
+        dataObj = JSON.parse(data);
+    } catch (e) {
+        dataObj = data;
+    }
+    let done = (result, callback) => {
+        if (callback) {
+            callback(result);
+        }
+        defer.resolve(result);
+    };
+    let failure = (result, callback) => {
+        if (callback) {
+            callback(result);
+        }
+        defer.reject(result);
+    };
+    return (callback) => {
+        if (typeof dataObj == "object") {
+            if (dataObj["errcode"] && dataObj["errcode"] != 0) {
+                failure(dataObj, callback);
+            } else {
+                done(dataObj.data, callback);
+            }
+            return;
+        }
+        done(dataObj, callback);
+    }
+};
+
 let pmAPI = {
     get: (path, callback) => {
+        path = addCompanyIdToPath(path);
         logger.info("Getting data from liveRoom API with path: " + path);
         let defer = new Deferred();
         request(baseUrl + path, (err, res, data) => {
@@ -18,40 +63,19 @@ let pmAPI = {
                 }
                 defer.reject(err);
             } else {
-                let dataObj = {};
-                try {
-                    dataObj = JSON.parse(data);
-                    if ("errcode" in dataObj && dataObj["errcode"] != 0) {
-                        defer.reject(dataObj);
-                        if (callback) {
-                            callback(dataObj);
-                            return defer.promise;
-                        }
-                    }
-                    if (callback) {
-                        callback(dataObj.data);
-                    }
-                    defer.resolve(dataObj.data);
-                } catch (e) {
-                    logger.error("Get " + path.split("?")[0] + ">>>data:" + data, e);
-                    if (callback) {
-                        callback(true);
-                    }
-                    defer.reject(e);
-                }
+                doRequestSuccess(data, defer)(callback);
             }
         });
         return defer.promise;
     },
     post: (path, data, callback) => {
+        data = addCompanyIdToBody(data);
         logger.info("Posting data to liveRoom API with path: " + path);
         let defer = new Deferred();
         request.post({
             url: baseUrl + path,
             body: data,
-            headers: {
-                "Connection": "close"
-            },
+            headers: { "Connection": "close" },
             json: true
         }, (err, res, data) => {
             if (err) {
@@ -61,27 +85,7 @@ let pmAPI = {
                 }
                 defer.reject(err);
             } else {
-                let dataObj = {};
-                try {
-                    dataObj = typeof data === "object" ? data : JSON.parse(data);
-                    if ("errcode" in dataObj && dataObj["errcode"] != 0) {
-                        defer.reject(dataObj);
-                        if (callback) {
-                            callback(dataObj);
-                            return defer.promise;
-                        }
-                    }
-                    if (callback) {
-                        callback(dataObj.data);
-                    }
-                    defer.resolve(dataObj.data);
-                } catch (e) {
-                    logger.error("Post " + path + ">>>data:" + JSON.stringify(data), e);
-                    if (callback) {
-                        callback(true);
-                    }
-                    defer.reject(e);
-                }
+                doRequestSuccess(data, defer)(callback);
             }
         });
         return defer.promise;
